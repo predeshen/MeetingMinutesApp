c9b7a8cbb3431aff0c61090fca0a0c65cce47730
Predeshen Naidoo <Predeshen.Naidoo@derivco.co.za>
2024-08-23 19:33:26 +0200

    Adding infrastructure layer bare bones

---
+++ Infrastructure/Data/ResolutionActionContext.cs
@@ -0,0 +1,21 @@
+﻿using MeetingMinutesApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingMinutesApp.Infrastructure.Data
+{
+    public class ResolutionActionContext : DbContext
+    {
+        public DbSet<MeetingType> MeetingTypes { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<MeetingItem> MeetingItems { get; set; }
+        public DbSet<MeetingItemStatus> MeetingItemStatuses { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        public ResolutionActionContext(DbContextOptions<ResolutionActionContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //To Do : Add relationships and constraints
+        }
+    }
+}
