9b427417e824fc62bed1e6e9fc36978efba2e074
Predeshen Naidoo <Predeshen.Naidoo@derivco.co.za>
2024-08-28 13:40:46 +0200

    Bug fix

--- Program.cs
+++ Program.cs
@@ -1,82 +1,75 @@
+using FluentValidation.AspNetCore;
+using FluentValidation;
 using MeetingMinutesApp.Application.UseCases;
+using MeetingMinutesApp.Application.Validators;
 using MeetingMinutesApp.Infrastructure.Data;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using MeetingMinutesApp.Application.Validators;
-using FluentValidation;
-using FluentValidation.AspNetCore;
 
-internal class Program
-{
-    private static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CaptureNewMeetingValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateMeetingItemStatusValidator>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        builder.Services.AddControllers()
-            .AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-            });
-        builder.Services.AddFluentValidationAutoValidation();
-        builder.Services.AddValidatorsFromAssemblyContaining<CaptureNewMeetingValidator>();
-        builder.Services.AddValidatorsFromAssemblyContaining<UpdateMeetingItemStatusValidator>();
+// Register DbContext
+builder.Services.AddDbContext<MeetingMinutesAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+// Register services
+builder.Services.AddScoped<CaptureNewMeeting>();
+builder.Services.AddScoped<UpdateMeetingItemStatus>();
 
-        // Register DbContext
-        builder.Services.AddDbContext<MeetingMinutesAppContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
 
-        // Register services
-        builder.Services.AddScoped<CaptureNewMeeting>();
-        builder.Services.AddScoped<UpdateMeetingItemStatus>();
-
-        var app = builder.Build();
-
-        // Initialize the database
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            try
-            {
-                var context = services.GetRequiredService<MeetingMinutesAppContext>();
-                DbInitializer.Initialize(context);
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred while seeding the database.");
-            }
-        }
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-
-        //Link React UI
-        app.UseSpa(spa =>
-        {
-            spa.Options.SourcePath = "meetingminutesui";
-
-            if (app.Environment.IsDevelopment())
-            {
-                spa.UseReactDevelopmentServer(npmScript: "start");
-            }
-        });
-
-        app.Run();
+// Initialize the database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<MeetingMinutesAppContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
     }
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+//Link React UI
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "meetingminutesui";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+});
+
+app.Run();
